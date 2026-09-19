roytam1/GreenPad

commit b7b6bc6f43e123f99956bba6c12b0af6bb584101

opencode muse spark 1.3 free suggested to merge following tokens into the same draw call to speed-up GDI drawing

editwing/ip_draw.cpp
@@ -622,6 +622,36 @@ void ViewImpl::DrawTXT( const VDrawInfo& v, Painter& p )
 					while( n<end && (flg[n]>>5)==0 )
 						++n;
 
+				// Merge following same-color plain-text tokens into
+				// this draw call. Tab/space/U+3000 runs keep their own
+				// drawing, and color changes still split. Pixels are
+				// identical, GDI calls much fewer on long lines.
+				if( str[i]!=L'\t' && str[i]!=L' ' && str[i]!=0x3000 )
+				{
+					ulong nn;
+					bool hsp = p.sc(scHSP);
+					bool zsp = p.sc(scZSP);
+					while( n<end )
+					{
+						if( str[n]==L'\t' )
+							break;
+						if( str[n]==L' ' && hsp )
+							break;
+						if( str[n]==0x3000 && zsp )
+							break;
+						if( (flg[n]&3) != (flg[i]&3) )
+							break;
+						t = (flg[n]>>5);
+						nn = n + t;
+						if( nn >= end )
+							{ n = end; break; }
+						if( t==7 || t==0 )
+							while( nn<end && (flg[nn]>>5)==0 )
+								++nn;
+						n = nn;
+					}
+				}
+
 				// x2, i2 := ‚±‚ÌToken‚Ì‰E’[
 				i2 ++;
 				x2 = (str[i]==L'\t' ? p.nextTab(x2) : x2+p.W(&str[i]));

commit 2e2221b9139b0ef619e349645e82e71fb316f6bd

opencode muse spark 1.3 free fixed display regression introduced in rev b7b6bc6f

editwing/ip_draw.cpp
@@ -686,11 +686,13 @@ void ViewImpl::DrawTXT( const VDrawInfo& v, Painter& p )
 					continue;
 
 				// x, i := このトークンの左端
+				unicode first = str[i];
+				ulong i0 = i;
 				if( x<v.XMIN )
 				{
 					// tabの分が戻りすぎ？
 					x = x2, i = i2;
-					while( v.XMIN<x )
+					while( v.XMIN<x && i>i0 )
 						x -= p.W( &str[--i] );
 				}
 
@@ -700,7 +702,7 @@ void ViewImpl::DrawTXT( const VDrawInfo& v, Painter& p )
 				p.Fill( a );
 
 				// 描画
-				switch( str[i] )
+				switch( first )
 				{
 				case L'\t':
 					if( p.sc(scTAB) )